b9c2ecd21eb5e4d426d37be9e73674835f974c65
Keith <kwakeham@gmail.com>
2023-03-16 11:46:56 -0600

    remove joystick and menu

--- thermo/thermo.ino
+++ thermo/thermo.ino
@@ -12,9 +12,6 @@ Frigidbear
 
 #define compressor_rest_time 60000 //5 minutes?
 
-#define joyx A0
-#define joyy A1
-#define threshold 200
 
 #define compressor_pin 12
 #define heater_pin 13
@@ -90,10 +87,6 @@ void setup() {
   display.clearDisplay();
   display.display();
 
-  // establish whatever pin reads you need for joystick  
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-
   //ensure the MAX31855 responds
   Serial.println("MAX31855 test");
   // wait for MAX chip to stabilize
@@ -126,7 +119,7 @@ void setup() {
 void loop(void) {
 
   joytstickupdate();
-  // statusupdate("Waiting", currentstatus);
+  statusupdate("Waiting", currentstatus);
   valueupdate();
   menuselect();
   readtemp();
@@ -146,119 +139,29 @@ void temperature_control(void)
       case idle_state:
         break;
       case cooling_state:
-        analogWrite(heater_pin, 0); //ensure heater is off
-        if (chamber_temp >= cold_temp)
-        {
-          compressor_state = true;
-        } else if (chamber_temp+1 < cold_temp)
-        {
-          compressor_state = false;
-        }
-
-        //if the temperature mets requirements, start timer and check
-        if (chamber_temp+1 < cold_temp)
-        {
-          if(temp_stable)
-          {
-            if(millis() > (temp_stable_time + (long)cold_time*1000*60))
-            {
-              temp_stable = false;
-              chamber_state = heating_state;
-              myController.start(); //start the heating controller
-            }
-          }
-          else
-          {
-            temp_stable = true;
-            temp_stable_time = millis();       
-          }
-        }
-        Serial.println("coolingstate");
         break;
       case heating_state:
-        compressor_state = false;
-        //if the temperature mets requirements, start timer and check
-        input = chamber_temp;
-        setpoint = hot_temp;
-        myController.compute();
-        analogWrite(heater_pin, output);
-        if (chamber_temp >= hot_temp-1)
-        {
-          if(temp_stable)
-          {
-            if(millis() > (temp_stable_time + (long)hot_time*1000*60))
-            {
-              temp_stable = false;
-              chamber_state = cooling_state;
-              myController.stop(); 
-            }
-          }
-          else
-          {
-            temp_stable = true;
-            temp_stable_time = millis();
-          }
-        }
-        Serial.print(output);
-        Serial.println(" ,heatingstate");
         break;
       default:
-        // statements
         break;
     }
   }
   else //if not running make sure to shut off compressor
   {
     compressor_state = false;
-    analogWrite(heater_pin, 0); //ensure heater is off
+    
+    compressor_control(compressor_state);
   }
-
-  compressor_control(compressor_state);
 }
 
 void compressor_control(bool run_compressor)
 {
-  bool actualrun = false;
-  if(run_compressor) //turn on compressor
-  {
-    if(compressor_running)
-    {
-      //should have enabled from before
-      actualrun = true;
-    } else
-    { //check to see if we can turn on the compressor
-      if (millis() > (compressor_stop_time + compressor_rest_time))
-      {
-        digitalWrite(compressor_pin, HIGH);
-        actualrun = true;
-        compressor_running = true;
-      }
 
-    }
-  }
-  else //turn off compressors
-  {
-    if(compressor_running)
-    { //get time
-      compressor_stop_time = millis();
-      digitalWrite(compressor_pin, LOW);
-      actualrun = false;
-    } else
-    {
-      //ensure it's still off?
-      digitalWrite(compressor_pin, LOW);
-      actualrun = false;
-    }
-    compressor_running = false;
-    
-  }
+}
 
-  Serial.print("temp: ");
-  Serial.print(chamber_temp);
-  Serial.print(" compressor: ");
-  Serial.print(run_compressor);
-  Serial.print(" compressor_actual: ");
-  Serial.println(actualrun);
+void compressor_control(uint heatvalue)
+{
+  analogWrite(heater_pin, 0); //ensure heater is off
 }
 
 void readtemp(void)
@@ -277,88 +180,9 @@ void readtemp(void)
   }
 }
 
-void joytstickupdate(void)
-{
-  int x = analogRead(joyx);
-  int y = analogRead(joyy);
-  if(x > (512+threshold))
-  {
-      menu_xmove = 1;
-  }
-  
-  if(x < (512-threshold))
-  {
-      menu_xmove = -1;
-  }
-
-  if(y > (512+threshold))
-  {
-      menu_ymove = 1;
-  }
-  
-  if(y < (512-threshold))
-  {
-      menu_ymove = -1;
-  }
-
-  //prevent cursor movment unless exit
-  if(system_running)
-  {menu_ymove = 0;}
-
-  menu_ypos = menu_ypos+menu_ymove;
-  menu_ymove = 0;
-  //Max out menus
-  if(menu_ypos > 4)
-  {menu_ypos = 4;}
-  if(menu_ypos < 0)
-  {menu_ypos = 0;}
-}
-
-void valueupdate()
-{
-  if(!system_running)
-  {
-    switch (menu_ypos) {
-      case 0:
-        cold_temp = cold_temp+menu_xmove;
-        break;
-      case 1:
-        cold_time = cold_time+menu_xmove;
-        break;
-      case 2:
-        hot_temp = hot_temp+menu_xmove;
-        break;
-      case 3:
-        hot_time = hot_time+menu_xmove;
-        break;
-      case 4:
-        if(menu_xmove>0)
-        {
-          system_running = true;
-          chamber_state = cooling_state;
-          myController.start();
-        }
-        break;
-      default:
-        // statements
-        break;
-    }    
-  }
-  else
-  {
-    if (menu_xmove<0)
-    {
-      system_running = false;
-      chamber_state = idle_state;
-    }
-  }
-  menu_xmove = 0;
-}
-
 void statusupdate(String currentstatus, bool runningstatus)
 {
   display.setTextSize(1);             // Normal 1:1 pixel scale
-  
   if(system_running)
   {
     display.clearDisplay();
@@ -376,77 +200,8 @@ void statusupdate(String currentstatus, bool runningstatus)
     display.setCursor(30,4);             // Start at top-left corner
     display.print(currentstatus);
   }
-    
 }
 
-void menuselect()
-{
-  //Cold
-  display.setTextColor(WHITE,BLACK); 
-  display.setCursor(4,16);             // Start at top-left corner
-  display.print(F("COLD"));
-  if(menu_ypos == 0)
-  {
-    display.setTextColor(BLACK,WHITE);        // Draw white text
-  } else
-  {
-    display.setTextColor(WHITE,BLACK);        // Draw white text
-  }
-  display.setCursor(4,28);             // Start at top-left corner
-  display.print(cold_temp);
-  display.print(F("c"));
-  display.setTextColor(WHITE,BLACK); 
-  display.setCursor(4,40);             // Start at top-left corner
-  display.print(F("TIME"));
-  if(menu_ypos == 1)
-  {
-    display.setTextColor(BLACK,WHITE);        // Draw white text
-  } else
-  {
-    display.setTextColor(WHITE,BLACK);        // Draw white text
-  }
-  display.setCursor(4,52);             // Start at top-left corner
-  display.print(cold_time);
-  display.print(F("m"));
 
-  //Hot
-  display.setTextColor(WHITE,BLACK);        // Draw white text
-  display.setCursor(40,16);             // Start at top-left corner
-  display.print(F("HOT"));
-  if(menu_ypos == 2)
-  {
-    display.setTextColor(BLACK,WHITE);        // Draw white text
-  } else
-  {
-    display.setTextColor(WHITE,BLACK);        // Draw white text
-  }
-  display.setCursor(40,28);             // Start at top-left corner
-  display.print(hot_temp);
-  display.print(F("c"));
-  display.setTextColor(WHITE,BLACK);        // Draw white text
-  display.setCursor(40,40);             // Start at top-left corner
-  display.print(F("TIME"));
-  if(menu_ypos == 3)
-  {
-    display.setTextColor(BLACK,WHITE);        // Draw white text
-  } else
-  {
-    display.setTextColor(WHITE,BLACK);        // Draw white text
-  }
-  display.setCursor(40,52);             // Start at top-left corner
-  display.print(hot_time);
-  display.print(F("m"));
-
-  //START
-  if(menu_ypos == 4)
-  {
-    display.setTextColor(BLACK,WHITE);        // Draw white text
-  } else
-  {
-    display.setTextColor(WHITE,BLACK);        // Draw white text
-  }
-  display.setCursor(76,16);             // Start at top-left corner
-  display.print(F("START"));
-}
 
 /////////////////////////////////////////////////////////
